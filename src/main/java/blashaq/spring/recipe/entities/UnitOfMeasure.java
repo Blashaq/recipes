d6657972bbc364533e2b496ffe538358093be49f
ablasiak <212718@student.pwr.edu.pl>
2018-07-10 16:43:58 +0200

    added recipe entities

---
+++ src/main/java/blashaq/spring/recipe/entities/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package blashaq.spring.recipe.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String unit;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
